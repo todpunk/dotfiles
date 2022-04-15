4f13875747fddef879a8dc4f4cc5241236502948
Tod Hansmann <githubpub@todandlorna.com>
2022-04-14 18:46:35 -0600

    feat: migrate to localvars settings usage

--- UpdateCloneGithubStuff.sh
+++ UpdateCloneGithubStuff.sh
@@ -3,22 +3,22 @@
 set -e
 #set -x
 
+# localvars is the preferred storage for config for our utilities
+if [[ -f "${HOME}/.localvars" ]]; then
+    source "${HOME}/.localvars"
+fi
 # Expects a directory where the structure is <DIR>/<Orgs>/<Repos>/
-REPOSDIR="${HOME}/repos"
-# Obviously change this to your user
-GIT_USER="todpunk"
+REPOSDIR=${REPOSDIR:-'${HOME}/repos'}
+# Obviously change this to your user, this is for auth
+GITHUB_USER=${GITHUB_USER:-'todpunk'}
 # Your user is included by default, but you can adjust that as well.
-USERS="${GIT_USER}"
-ORGS="matthewsreis catalystsquad tnlcommunity"
+GITHUB_USERS=${GITHUB_USERS:-'${GITHUB_USER}'}
+GITHUB_ORGS=${GITHUB_ORGS:-'catalystsquad tnlcommunity'}
 PERPAGE=10
 
-if [[ -z "$GIT_PAT" ]]; then
-    echo "GIT_PAT not set, sourcing .localvars file. Maybe I'm in a cron"
-    source "${HOME}/.localvars"
-    if [[ -z "$GIT_PAT" ]]; then
-        echo "Must provide GIT_PAT in environment" 1>&2
-        exit 1
-    fi
+if [[ -z "$GITHUB_PAT" ]]; then
+    echo "Must provide GITHUB_PAT in environment" 1>&2
+    exit 1
 fi
 
 # First update anything we've got already
@@ -57,12 +57,12 @@ echo "Done with updates"
 tmpfile=$(mktemp /tmp/gitupdater.XXXXXX)
 
 echo "Checking Repos in Users"
-for user in $USERS 
+for user in ${GITHUB_USERS} 
 do
   echo "Checking repos for user: ${user}"
   BASEURL="https://api.github.com/users/${user}/repos"
   set +e
-  LINKS=`curl -I -i -u "${GIT_USER}:${GIT_PAT}" -H "Accept: application/vnd.github.v3+json" -s ${BASEURL}?per_page=${PERPAGE} | grep -i link: 2>/dev/null`
+  LINKS=`curl -I -i -u "${GITHUB_USER}:${GITHUB_PAT}" -H "Accept: application/vnd.github.v3+json" -s ${BASEURL}?per_page=${PERPAGE} | grep -i link: 2>/dev/null`
   set -e
   if [ -z "$LINKS" ]
   then
@@ -74,7 +74,7 @@ do
   i=1
   until [ $i -gt $TOTALPAGES ]
   do
-    RESULT=`curl -s -u "${GIT_USER}:${GIT_PAT}" "${BASEURL}?per_page=${PERPAGE}&page=${i}" 2>&1`
+    RESULT=`curl -s -u "${GITHUB_USER}:${GITHUB_PAT}" "${BASEURL}?per_page=${PERPAGE}&page=${i}" 2>&1`
     echo ${RESULT} > ${tmpfile}
     mkdir -p $user
     cd $user
@@ -94,13 +94,13 @@ do
 done
 
 echo "Checking Repos in Orgs"
-for org in $ORGS 
+for org in ${GITHUB_ORGS}
 do
   echo "Checking repos for org: ${org}"
   BASEURL="https://api.github.com/orgs/${org}/repos"
   echo "Forming curl command"
   set +e
-  LINKS=`curl -I -i -u "${GIT_USER}:${GIT_PAT}" -H "Accept: application/vnd.github.v3+json" -s ${BASEURL}?per_page=${PERPAGE} | grep -i link: 2>/dev/null`
+  LINKS=`curl -I -i -u "${GITHUB_USER}:${GITHUB_PAT}" -H "Accept: application/vnd.github.v3+json" -s ${BASEURL}?per_page=${PERPAGE} | grep -i link: 2>/dev/null`
   set -e
   if [ -z "$LINKS" ]
   then
@@ -112,7 +112,7 @@ do
   i=1
   until [ $i -gt $TOTALPAGES ]
   do
-    RESULT=`curl -s -u "${GIT_USER}:${GIT_PAT}" "${BASEURL}?per_page=${PERPAGE}&page=${i}" 2>&1`
+    RESULT=`curl -s -u "${GITHUB_USER}:${GITHUB_PAT}" "${BASEURL}?per_page=${PERPAGE}&page=${i}" 2>&1`
     echo ${RESULT} > ${tmpfile}
     # Debug stuff I wanted to leave for posterity to fiddle with if the API changes
     #echo "Repo Name, SSH URL, Clone URL"
