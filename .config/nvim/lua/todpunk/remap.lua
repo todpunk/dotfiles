51dad5e20973399773558173fdd7b658b6c10140
Tod Hansmann <githubpub@todandlorna.com>
2023-01-14 15:14:26 -0700

    fix: tree-sitter-cli and copy/paste functionality

--- .config/nvim/lua/todpunk/remap.lua
+++ .config/nvim/lua/todpunk/remap.lua
@@ -1,10 +1,11 @@
 vim.g.mapleader = " "
 -- Normal modes
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<C-v>", ":r ~/.vimbuffer<CR>")
 
 
 -- Visual modes
-
+vim.keymap.set("v", "<C-c>", "y:new ~/.vimbuffer<CR>VGp:x<CR> | :!cat ~/.vimbuffer | clip.exe <CR><CR>")
 
 -- X modes
 vim.keymap.set("x", "<leader>p", "\"_dP")
