a228d4042af7c00dfe32a5594f6baba50a620499
Tod Hansmann <githubpub@todandlorna.com>
2023-01-14 15:22:17 -0700

    fix: add event based copy for yanking instead of just C-c

--- .config/nvim/lua/todpunk/remap.lua
+++ .config/nvim/lua/todpunk/remap.lua
@@ -6,6 +6,10 @@ vim.keymap.set("n", "<C-v>", ":r ~/.vimbuffer<CR>")
 
 -- Visual modes
 vim.keymap.set("v", "<C-c>", "y:new ~/.vimbuffer<CR>VGp:x<CR> | :!cat ~/.vimbuffer | clip.exe <CR><CR>")
+-- Also a yank event version
+vim.api.nvim_create_autocmd("TextYankPost", {
+    command = "* call system('echo '.shellescape(join(v:event.regcontents, \"\\<CR>\")).' |  clip.exe')"
+})
 
 -- X modes
 vim.keymap.set("x", "<leader>p", "\"_dP")
