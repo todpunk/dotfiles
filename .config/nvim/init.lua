913484001cb9255f377b1c0ff90a751cfed66e2d
Tod Hansmann <githubpub@todandlorna.com>
2024-05-27 11:24:35 -0600

    fix: added rye, removed git from kickstart

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -841,5 +841,6 @@ require('lazy').setup({
   },
 })
 
+vim.keymap.set('n', '<leader>pv', '<cmd>Explore<CR>', {})
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- .profile
+++ .profile
@@ -21,3 +21,4 @@ fi
 
 # Generated for envman. Do not edit.
 [ -s "$HOME/.config/envman/load.sh" ] && source "$HOME/.config/envman/load.sh"
+. "$HOME/.rye/env"
